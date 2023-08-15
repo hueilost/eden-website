7cd81dbf123222f30bd450b46d7cb25251a1bc7e
AlexDev404 <AlexDev404@users.noreply.github.com>
2023-08-15 12:24:43 -0600

    EJS Template+Live reloading

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 8080;
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+const liveReloadServer = livereload.createServer();
 
+// Setup live reloading
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+// Use this middleware
+app.use(connectLiveReload());
 // Create static dir
 app.use(express.static(path.join("./static/")));
 
